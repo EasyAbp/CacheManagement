9c3cff3f5b1745acd29784d9b68340664d5071ed
gdlcf88 <gdlcf88@outlook.com>
2020-10-21 16:58:49 +0800

    Upgrade to ABP v3.2.1

--- host/EasyAbp.CacheManagement.Web.Host/CacheManagementWebHostModule.cs
+++ host/EasyAbp.CacheManagement.Web.Host/CacheManagementWebHostModule.cs
@@ -56,6 +56,7 @@ namespace EasyAbp.CacheManagement
         typeof(AbpTenantManagementWebModule),
         typeof(AbpTenantManagementHttpApiClientModule),
         typeof(AbpFeatureManagementHttpApiClientModule),
+        typeof(AbpFeatureManagementWebModule),
         typeof(AbpPermissionManagementHttpApiClientModule),
         typeof(AbpAspNetCoreSerilogModule)
         )]
