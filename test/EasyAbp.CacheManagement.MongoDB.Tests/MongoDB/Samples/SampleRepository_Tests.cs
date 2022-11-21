d0cd224ae7b171fb2b933937da288849e9ba0141
gdlcf88 <gdlcf88@outlook.com>
2022-11-21 21:53:12 +0800

    Fix test projects

--- test/EasyAbp.CacheManagement.MongoDB.Tests/MongoDB/Samples/SampleRepository_Tests.cs
+++ test/EasyAbp.CacheManagement.MongoDB.Tests/MongoDB/Samples/SampleRepository_Tests.cs
@@ -1,7 +1,9 @@
 ﻿using EasyAbp.CacheManagement.Samples;
+using Xunit;
 
 namespace EasyAbp.CacheManagement.MongoDB.Samples
 {
+    [Collection(MongoTestCollection.Name)]
     public class SampleRepository_Tests : SampleRepository_Tests<CacheManagementMongoDbTestModule>
     {
         /* Don't write custom repository tests here, instead write to
