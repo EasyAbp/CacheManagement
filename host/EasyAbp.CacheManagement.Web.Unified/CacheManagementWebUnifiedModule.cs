aaf5c3d319b30a23d7b98f5d12ef620b6d5f5ce1
gdlcf88 <gdlcf88@outlook.com>
2023-01-14 14:01:52 +0800

    Upgrade to ABP 7.0.0

--- host/EasyAbp.CacheManagement.Web.Unified/CacheManagementWebUnifiedModule.cs
+++ host/EasyAbp.CacheManagement.Web.Unified/CacheManagementWebUnifiedModule.cs
@@ -34,7 +34,9 @@ using Volo.Abp.PermissionManagement;
 using Volo.Abp.PermissionManagement.EntityFrameworkCore;
 using Volo.Abp.PermissionManagement.HttpApi;
 using Volo.Abp.PermissionManagement.Identity;
+using Volo.Abp.SettingManagement;
 using Volo.Abp.SettingManagement.EntityFrameworkCore;
+using Volo.Abp.SettingManagement.Web;
 using Volo.Abp.Swashbuckle;
 using Volo.Abp.TenantManagement;
 using Volo.Abp.TenantManagement.EntityFrameworkCore;
@@ -54,6 +56,9 @@ namespace EasyAbp.CacheManagement
         typeof(AbpAccountWebModule),
         typeof(AbpAccountApplicationModule),
         typeof(AbpEntityFrameworkCoreSqlServerModule),
+        typeof(AbpSettingManagementApplicationModule),
+        typeof(AbpSettingManagementHttpApiModule),
+        typeof(AbpSettingManagementWebModule),
         typeof(AbpSettingManagementEntityFrameworkCoreModule),
         typeof(AbpPermissionManagementEntityFrameworkCoreModule),
         typeof(AbpPermissionManagementApplicationModule),
