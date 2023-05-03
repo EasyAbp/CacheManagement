2ad398c0c87b98e5488ae091998e1d1cebe614d7
gdlcf88 <gdlcf88@outlook.com>
2023-05-03 21:18:25 +0800

    Upgrade to ABP 7.2.1

--- test/EasyAbp.CacheManagement.EntityFrameworkCore.Tests/EntityFrameworkCore/CacheManagementEntityFrameworkCoreTestModule.cs
+++ test/EasyAbp.CacheManagement.EntityFrameworkCore.Tests/EntityFrameworkCore/CacheManagementEntityFrameworkCoreTestModule.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.Modularity;
+using Volo.Abp.Uow;
 
 namespace EasyAbp.CacheManagement.EntityFrameworkCore
 {
@@ -15,6 +16,7 @@ namespace EasyAbp.CacheManagement.EntityFrameworkCore
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            context.Services.AddAlwaysDisableUnitOfWorkTransaction();
             var sqliteConnection = CreateDatabaseAndGetConnection();
 
             Configure<AbpDbContextOptions>(options =>
